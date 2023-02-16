75f35180f4a2a63abdf925ccb3d073ab584beb91
The-forecaster <gerodudev2@gmail.com>
2023-02-15 21:31:46 -0700

    updated ExampleMod.kt

--- src/main/kotlin/com/example/modid/ExampleMod.kt
+++ src/main/kotlin/com/example/modid/ExampleMod.kt
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class ExampleMod : ModInitializer {
-    companion object {
+	companion object {
 		// This logger is used to write text to the console and the log file.
 		// It is considered best practice to use your mod id as the logger's name.
 		// That way, it's clear which mod wrote info, warnings, and errors.
