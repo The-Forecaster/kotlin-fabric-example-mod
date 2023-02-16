912505caf4dc1699c9906f3b226867d6d28f3a88
The-forecaster <gerodudev2@gmail.com>
2023-02-15 17:41:32 -0700

    fixed whitespace

--- src/main/kotlin/com/example/modid/ExampleMod.kt
+++ src/main/kotlin/com/example/modid/ExampleMod.kt
@@ -6,18 +6,18 @@ import org.slf4j.LoggerFactory;
 
 class ExampleMod : ModInitializer {
     companion object {
-        // This logger is used to write text to the console and the log file.
-	    // It is considered best practice to use your mod id as the logger's name.
-	    // That way, it's clear which mod wrote info, warnings, and errors.
+		// This logger is used to write text to the console and the log file.
+		// It is considered best practice to use your mod id as the logger's name.
+		// That way, it's clear which mod wrote info, warnings, and errors.
 		@JvmField
-	    val LOGGER: Logger = LoggerFactory.getLogger("modid");
+		val LOGGER: Logger = LoggerFactory.getLogger("modid");
     }
 
     override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		
 		LOGGER.info("Hello Fabric world!");
 	}
 }
